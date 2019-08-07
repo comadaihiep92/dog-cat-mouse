09c3d1ef270338caace2f739265546bb96a483c7
Stork <motden100@gmail.com>
2019-08-07 15:23:15 +0700

    sayHi method for Dog

--- Dog2.js
+++ Dog2.js
@@ -1,7 +1,17 @@
+// import chalk from "chalk";
+var chalk = require('chalk');
+
 function Dog2() {
+    this.name = name;
     this.stomach = [];
 }
 
-Dog.prototype.eat = function(cat) {
+Dog2.prototype.eat = function(cat) {
     this.stomach.push(cat);
 }
+
+Dog2.prototype.sayHi = function() {
+    console.log('Hi! I am a dog. My name is ' + chalk.red(this.name));
+}
+
+module.exports = Dog2;
